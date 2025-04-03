708afa9e39371a3505e45dd7a073a9b4738959d2
It developer 😎 <159410366+Nukesul@users.noreply.github.com>
2025-04-03 10:31:29 +0600

    211

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Changed from 'dist' to 'build'
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name]-[hash][extname]",
-        chunkFileNames: "assets/[name]-[hash].js",
-        entryFileNames: "assets/[name]-[hash].js",
-      },
-    },
-  },
-  server: {
-    headers: {
-      "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
-      Pragma: "no-cache",
-      Expires: "0",
-      "Surrogate-Control": "no-store",
-    },
+    outDir: 'build', 
   },
 });
